d96ced123a152e23d2d13b41bd2a5d1764981482
aidaidaid <aidashirinovaaa@gmail.com>
2021-08-19 01:39:40 +0400

    7-12

--- script.js
+++ script.js
@@ -133,8 +133,25 @@
 
 //12
 var numbers = [7, 21, 5, 10, 8];
-numbers.sort(function(a, b) {
-  return a - b;
-}
-);
+numbers.sort((a, b) => a - b)
+// numbers.sort(function(a, b) {
+//   return a - b;
+// }
+// );
 console.log(numbers);
+
+function binarySearch(sortedArr, ourNum) {
+    let start = 0;
+    let end = sortedArr.length - 1;
+
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+        if (sortedArr[middle] === ourNum) {
+            return middle;
+        } else if (sortedArr[middle] < ourNum) {
+            start = middle + 1;
+        } else {
+            end = middle - 1;
+        }
+    }
+}
