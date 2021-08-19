e10b89ebb89ad1351317076591c01af5d7c273a2
aidaidaid <aidashirinovaaa@gmail.com>
2021-08-19 21:58:04 +0400

    done

--- script.js
+++ script.js
@@ -28,23 +28,31 @@
 
 //2
 // let num = prompt('Введите число:');
-
-// if (num % 2 == 0) {
-//     alert('Число ' + num + ' четное');
-// } else {
-//     alert('Число ' + num + ' нечетное');
-// }
+// if ((num !== '') && (typeof Number(num) == 'number') && (!isNaN(num))) {
+//     if (num % 2 == 0) {
+//         alert(`Число ${num} четное`);
+//     } else {
+//         alert(`Число ${num} нечетное`);
+//     }
+// } else alert('Вы не ввели число');
 
 //3
 // let num1 = prompt('Введите первое число:');
-// let num2 = prompt('Введите второе число:');
-
-// if (num1 > num2) {
-//     alert('Число ' + num1 + ' больше, чем ' + num2);
-// } else if (num1 < num2) {
-//     alert('Число ' + num2 + ' больше, чем ' + num1);
+// if ((num1 == '') || (typeof Number(num1) !== 'number') || (isNaN(num1))) {
+//     alert('Вы не ввели число');
 // } else {
-//     alert('Числа равны');
+//     let num2 = prompt('Введите второе число:');
+//     if ((num2 == '') || (typeof Number(num2) !== 'number') || (isNaN(num2))) {
+//         alert('Вы не ввели число');
+//     } else {
+//         if (num1 > num2) {
+//             alert('Число ' + num1 + ' больше, чем ' + num2);
+//         } else if (num1 < num2) {
+//             alert('Число ' + num2 + ' больше, чем ' + num1);
+//         } else {
+//             alert('Числа равны');
+//         }
+//     }
 // }
 
 //4
@@ -55,6 +63,7 @@
 // console.log(arr1);
 // console.log(arr2);
 // console.log(arr3);
+// console.log(`В объедненном массиве ${arr3.length} элементов`);
 
 //5
 // const arr = [2, 5, 8, 1, 10];
@@ -67,25 +76,33 @@
 
 //6
 // function match() {
-//     let a = prompt('Введите число от 1 до 10');
-//     let b = Math.floor(Math.random() * 10) + 1;
+//     let isGame = false;
+//     do {
+//         let a = prompt('Введите число от 1 до 10');
+//         let b = Math.floor(Math.random() * 10) + 1;
 
-//     if (a == b) alert('You guessed it right!');
-//     else alert('Sorry. You lost');
+//         if (a == b) {
+//             alert('You guessed it right!');
+//             isGame = false;
+//         } else {
+//             alert('Sorry. You lost');
+//             isGame = confirm("Хотите сыграть еще раз?");
+//         }
+
+//     } while (isGame)
 // }
 
 // match();
 
 //7
 // function dividers(num) {
-//     if (num>1000 && num>0) {
+//     if (num > 1000 && num > 0) {
 //         return console.log('Число должно быть не больше 1000');
 //     } else {
 //         let arr = [];
-//         for (let i = 1; i <= num; i++) { 
-//             if (num % i == 0) 
-//             {
-//                arr.push(i);
+//         for (let i = 1; i <= num; i++) {
+//             if (num % i == 0) {
+//                 arr.push(i);
 //             }
 //         }
 //         return arr;
@@ -93,65 +110,88 @@
 // }
 
 //8
-// function cubed(n) {
-//     return Math.pow(n, 3);
+// function cube(num) {
+//     return Math.pow(num, 3);
 // }
 
 //9
-// function factorial(n) {
-//     if (n === 1) {
-//         return 1;
+// function factorial(num) {
+//     let total = 1;
+//     for (i = 0; i < num; i++) {
+//         total = total * (num - i);
 //     }
-
-//     return n * factorial(n - 1);
+//     return total;
 // }
 
 //10
 // function check(num) {
-
-// let isSimple = true;
-// for (i=2; i<num; i++) {
-//     if (num%i==0) {
-//         isSimple = false;
-//         break;
-//     }
-// }
-//     if (isSimple == true) {
-//         console.log(`Число ${num} простое`);
+//     if (num <= 1 || !Number.isInteger(num)) {
+//         console.log('К простым и составным чисам относятся только натуральные числа больше единицы')
 //     } else {
-//         console.log(`Число ${num} сложное`);
+//         let isSimple = true;
+//         for (i = 2; i < num; i++) {
+//             if (num % i == 0) {
+//                 isSimple = false;
+//                 break;
+//             }
+//         }
+
+//         if (isSimple) {
+//             console.log(`Число ${num} простое`);
+//         } else {
+//             console.log(`Число ${num} сложное`);
+//         }
 //     }
 // }
 
 //11
 // let arr = ['Ferrary', 'Mazda', 'Ford', 'Opel', 'Mazda', 'Toyota', 'Ford'];
 // console.log(arr);
-// arr = arr.filter(function(el, pos) {
-//     return arr.indexOf(el) == pos;
-// });
+// arr = arr.filter((el, pos) => (arr.indexOf(el) == pos));
 // console.log(arr);
 
 //12
-var numbers = [7, 21, 5, 10, 8];
-numbers.sort((a, b) => a - b)
-// numbers.sort(function(a, b) {
-//   return a - b;
+// var numbers = [7, 21, 5, 10, 8];
+// numbers.sort((a, b) => a - b)
+// // numbers.sort(function(a, b) {
+// //   return a - b;
+// // }
+// // );
+// console.log(numbers);
+
+// function binarySearch(sortedArr, ourNum) {
+//     let start = 0;
+//     let end = sortedArr.length - 1;
+
+//     while (start <= end) {
+//         let middle = Math.floor((start + end) / 2);
+//         if (sortedArr[middle] === ourNum) {
+//             return middle;
+//         } else if (sortedArr[middle] < ourNum) {
+//             start = middle + 1;
+//         } else {
+//             end = middle - 1;
+//         }
+//     }
 // }
-// );
-console.log(numbers);
 
-function binarySearch(sortedArr, ourNum) {
-    let start = 0;
-    let end = sortedArr.length - 1;
+// const numbers = [7, 21, 5, 10, 8];
+// console.log(numbers);
 
-    while (start <= end) {
-        let middle = Math.floor((start + end) / 2);
-        if (sortedArr[middle] === ourNum) {
-            return middle;
-        } else if (sortedArr[middle] < ourNum) {
-            start = middle + 1;
-        } else {
-            end = middle - 1;
-        }
-    }
-}
+// function bubble(arr) {
+//     let swapped, temp;
+//     do {
+//         swapped = false;
+//         for (var i = 0; i < arr.length - 1; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 temp = arr[i];
+//                 arr[i] = arr[i + 1];
+//                 arr[i + 1] = temp;
+//                 swapped = true;
+//             }
+//         }
+//     } while (swapped);
+// }
+
+// bubble(numbers);
+// console.log(numbers);
